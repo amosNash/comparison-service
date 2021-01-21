241dbd9c86618c86cf8189b7608d7a6f1b3f73d1
amosNash <66794449+amosNash@users.noreply.github.com>
2021-01-21 15:16:07 -0700

    Set-up repo

---
+++ client/src/app.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.component {
+  constructor () {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+
+
+
+  render () {
+    return (
+      <h1>HELLO!</h1>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
