7390defc9f45137bd185a385ebdec971e630e8f9
amosNash <66794449+amosNash@users.noreply.github.com>
2021-02-23 15:25:11 -0700

    Adding API calls

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -8,29 +8,54 @@ class App extends React.Component {
   constructor (props) {
     super (props);
     this.state = {
-      item1: null,
-      item2: null,
-      item3: null,
-      item4: null,
-      item5: null,
-      similarItems: []
+      ids: [],
+      photos: [],
+      names: [],
+      prices: [],
+      brands: []
     };
     this.getSimilarItems = this.getSimilarItems.bind(this);
+    this.getItemDescription = this.getItemDescription.bind(this);
   }
 
+  //get item info
+  getItemDescription (productIds) {
+    let item1 = productIds[0];
+    let item2 = productIds[1];
+    let item3 = productIds[2];
+    let item4 = productIds[3];
+    let item5 = productIds[4];
+    let item6 = productIds[5];
+    axios.get('http://ec2-18-217-85-161.us-east-2.compute.amazonaws.com:4004/descriptions/multiple', productIds)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
   //get items from comparison/getSimilarItems/comparison
-  getSimilarItems (productId) {
+  getSimilarItems (id) {
+    let productId = id;
+    let productIds = [];
+    productIds.push(productId);
+    this.setState({
+      ids: productIds
+    });
     axios.get(`http://localhost:4005/comparison/getSimilarItems/${productId}`)
       .then((response) => {
-        let items = response.data;
-        console.log(items);
+        let similarIds = response.data;
+        this.setState({
+          ids: productIds.concat(similarIds)
+        });
+        this.getItemDescription(this.state.ids);
       })
       .catch(err => {
         console.log(err);
       });
   }
 
-  //get info for each item
+  //get info for each item by sending request to description service, price service
 
 
   //componentDidMount shall call getSimilarItems
